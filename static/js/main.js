689039cc8ffa0cb709ea3c8dcb36abc875ad213a
Ann <i.annchoys@gmail.com>
2026-07-08 17:00:55 +0300

    Create responsive hero section

---
+++ static/js/main.js
@@ -0,0 +1,41 @@
+$(function () {
+    var $header = $(".hero__header");
+
+    function updateHeader() {
+        if ($(window).scrollTop() > 40) {
+            $header.addClass("hero__header--fixed");
+        } else {
+            $header.removeClass("hero__header--fixed");
+        }
+    }
+
+    updateHeader();
+
+    $(window).on("scroll", updateHeader);
+
+    $(".hero__anchor").on("click", function (event) {
+        var targetId = $(this).attr("href");
+        var $target = $(targetId);
+
+        if ($target.length === 0) {
+            return;
+        }
+
+        event.preventDefault();
+
+        var headerHeight = $header.outerHeight() || 0;
+        var targetPosition = Math.max(
+            $target.offset().top - headerHeight,
+            0
+        );
+
+        $("html, body").stop().animate(
+            {
+                scrollTop: targetPosition
+            },
+            600
+        );
+
+        $("#mainMenu").collapse("hide");
+    });
+});
