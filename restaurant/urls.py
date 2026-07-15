96411c0dd416d5655b6ebae79413264fd8f39860
Ann <i.annchoys@gmail.com>
2026-07-15 22:48:46 +0300

    Update restaurant urls

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -7,5 +7,6 @@ app_name = "restaurant"
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("booking/create/", views.booking_create, name="booking_create"),
     path("contact/create/", views.contact_create, name="contact_create"),
 ]
